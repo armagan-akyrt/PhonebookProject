ddfc954ef2ef8bab40e7b360a7a44b18421beec9
armagan-akyrt <akyurta@mef.edu.tr>
2023-08-02 13:23:20 +0300

    Deleted meeting functions
    Deletedmeetings page now works properly, a user is able to hard delete a soft-deleted meeting now.

---
+++ deletedMeetings.js
@@ -0,0 +1,27 @@
+import { fetchMeetings, softDeleteMeeting } from "./Scripts/ClientFunctionsMeeting.js";
+
+let currentUser = null;
+let startInterval = new Date();
+let endInterval = new Date();
+
+window.onload = function () {
+    currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+
+    startInterval.setUTCFullYear(startInterval.getUTCFullYear() - 100);
+    startInterval = startInterval.toISOString().replace('T', ' ').replace('Z', '');
+
+    endInterval.setUTCFullYear(endInterval.getUTCFullYear() + 100);
+    endInterval = endInterval.toISOString().replace('T', ' ').replace('Z', '');
+    
+    fetchMeetings('', false, startInterval, endInterval, currentUser.id);
+}
+
+document.getElementById('searchInputMeeting').addEventListener('keyup', function() {
+    let searchWord = document.getElementById('searchInputMeeting').value;
+    fetchMeetings(searchWord, false, startInterval, endInterval, currentUser.id);
+});
+
+document.getElementById('deleteMeetingButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    softDeleteMeeting();
+});
