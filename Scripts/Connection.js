2c9f6a62c1d6f64405ca2b34718220924077016c
armagan-akyrt <akyurta@mef.edu.tr>
2023-08-09 14:38:45 +0300

    connection string updated updated conections string so that it connects to a remote db instead of local db

--- Scripts/Connection.js
+++ Scripts/Connection.js
@@ -2,10 +2,16 @@
 const sql = require('mssql/msnodesqlv8');
 
 // SQL Server config
+// const config = {
+//     driver: 'msnodesqlv8',
+//     connectionString: 'Driver={SQL Server Native Client 11.0};Server=.;Database=PhoneDirectory;Trusted_Connection=yes;',
+// };
+
 const config = {
     driver: 'msnodesqlv8',
-    connectionString: 'Driver={SQL Server Native Client 11.0};Server=.;Database=PhoneDirectory;Trusted_Connection=yes;',
+    connectionString: 'Driver={SQL Server Native Client 11.0};Server=database-1.caajotm0qvd6.eu-central-1.rds.amazonaws.com;Database=PhoneDirectory;Uid=admin;Pwd=5weiapz7SQL;',
 };
+
 
 class Connection {
 
