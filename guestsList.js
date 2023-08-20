aec69d851fd88a62b7022f2b5c4df94877186740
armagan-akyrt <akyurta@mef.edu.tr>
2023-08-20 23:47:42 +0300

    completed guest listing.

--- guestsList.js
+++ guestsList.js
@@ -10,10 +10,11 @@ function fetchGuests(searchWord, startDate, endDate, companyName, isInside) {
     fetch(`/getguests?searchWord=${searchWord}&startDate=${startDate}&endDate=${endDate}&companyName=${companyName}&isInside=${isInside}`)
     .then(response => response.json())
     .then(data => {
-        guestsData = data;
+        guestsData = data.guestsList;
         renderGuests(data);
     })
     .catch(error => console.error('Error fetching guests:', error));
+
 }
 
 // Function to render guests in a table
@@ -21,14 +22,14 @@ function renderGuests(data) {
     const tbody = document.getElementById('gridBody');
     tbody.innerHTML = '';
     data.guestsList.forEach(item => {
-        tbody.innerHTML += renderRow(item);
+        tbody.innerHTML += renderRow(item, item.id);
     });
 }
 
 // Function to generate a table row for a guest
-function renderRow(item) {
+function renderRow(item, index) {
     return `
-        <tr>
+        <tr data-id="${index}">
             <td>${item.name}</td>
             <td>${item.surname}</td>
             <td>${item.companyName}</td>
@@ -41,11 +42,30 @@ function renderRow(item) {
     `;
 }
 
-// Function to set data from filter fields
 function setData() {
-    const searchWord = document.getElementById('usernameFilter').value;
-    const companyName = document.getElementById('companyNameFilter').value;
     // You can add other filter fields here as needed
+    let startDate = document.getElementById('startDateFilter').value;
+    let endDate = document.getElementById('endDateFilter').value;
+
+    let guestState = document.getElementById('guestStateFilter').value;
+    let isInside = guestState === 'false';
+
+    const companyName = document.getElementById('companyFilter').value;
+    const searchWord = document.getElementById('usernameFilter').value;
+
+    if (startDate == '') {  
+        startDate = new Date(2020, 0, 1);
+    }
+
+    if (endDate == '') {
+        endDate = new Date();
+    }
+
+    if (startDate > endDate) 
+    {
+        alert("Kart alım tarihi kart teslim tarihinden önce olamaz!");
+        return;
+    }
 
     fetchGuests(searchWord, startDate, endDate, companyName, isInside);
 }
@@ -58,12 +78,42 @@ window.onload = function() {
     // please write today - 1 year below
     const today = new Date();
     const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
-    fetchGuests('', oneYearAgo, today, '', '');
+
+    let guestState = document.getElementById('guestStateFilter').value;
+    let isInside = guestState === 'false';
+
+
+    fetchGuests('', oneYearAgo, today, '', isInside);
 }
 
-// Event listeners for filter fields
-document.getElementById('usernameFilter').addEventListener('keyup', setData);
-document.getElementById('companyNameFilter').addEventListener('keyup', setData);
+
+document.getElementById('filterButton').addEventListener('click', function() {
+    setData();
+
+});
+
+document.getElementById('getGuestCard').addEventListener('click', function() {
+    const selectedGuest = guestsData[selectedGuestIndex];
+    fetch('/obtainCard', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({guestId: selectedGuest.id, cardId: selectedGuest.cardId}),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert('Kart başarıyla teslim alındı!');
+        } else {
+            console.error('Kart teslim alınamadı:', data.message);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+
+});
+
 document.getElementById('deleteGuest').addEventListener('click', function() {
     // Delete guest logic; this can be expanded based on the backend API structure
     const selectedGuest = guestsData[selectedGuestIndex];
@@ -80,10 +130,41 @@ document.getElementById('deleteGuest').addEventListener('click', function() {
         if (data.success) {
             // Handle successful deletion, e.g., remove the guest from the list and re-render
         } else {
-            console.error('Error deleting guest:', data.message);
+            console.error('Misafir silinirken bir hata meydana geldi:', data.message);
         }
     })
     .catch(error => console.error('Error:', error));
 });
 
 // Additional logic and event listeners can be added as required
+
+document.addEventListener('DOMContentLoaded', function() {
+    const tbody = document.getElementById('gridBody');
+
+    // Attaching the event listener to the table body
+    tbody.addEventListener('click', function(event) {
+        let target = event.target;
+        while (target && target.tagName !== 'TR') {
+            target = target.parentElement;
+        }
+
+        if (target && target.dataset.id !== undefined) {
+            // Retrieve the guest data using the data-id attribute
+            const guestId = target.dataset.id;
+            const guest = guestsData.find(g => g.id == guestId);
+            
+            if (guest) {
+                // Populate the details in the right panel
+                document.getElementById('guestName').value = guest.name;
+                document.getElementById('guestSurname').value = guest.surname;
+                document.getElementById('guestCompanyName').value = guest.companyName;
+                document.getElementById('guestCardId').value = guest.cardId;
+                document.getElementById('guestVisitingUser').value = guest.visitingUser;
+                document.getElementById('guestVisitingUserFullName').value = guest.visiting.name + ' ' + guest.visiting.surname;
+                document.getElementById('guestCardGrantDate').value = guest.cardAcquisitionDate.replace('T', ' ').replace('Z', '');
+                document.getElementById('guestCardSubmitDate').value = guest.cardSubmitDate.replace('T', ' ').replace('Z', '');
+            }
+        }
+    });
+});
+
